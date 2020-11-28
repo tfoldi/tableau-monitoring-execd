3321d306ba93ecf5cc79109371d0ec2a67cb4ffc
Földi Tamás <tfoldi@xsi.hu>
2020-11-28 17:20:17 +0100

    implement influx protocol for systeminfo xml

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ fn get_epoch_ms() -> u128 {
         .as_millis()
 }
 
-fn get_system_info_xml(agent: &Agent, url: &String) -> Result<String,ureq::Error> {
+fn get_system_info_xml(agent: &Agent, url: &String) -> Result<String, ureq::Error> {
     let xml_server_info = agent.get(url)
         .call()?
         .into_string()?;
@@ -18,31 +18,35 @@ fn get_system_info_xml(agent: &Agent, url: &String) -> Result<String,ureq::Error
     Ok(xml_server_info)
 }
 
-fn parse_system_info(xml: &String) -> Result<(),roxmltree::Error>{
+fn parse_system_info(xml: &String) -> Result<(), roxmltree::Error> {
     let doc = roxmltree::Document::parse(&*xml)?;
 
     for node in doc.descendants() {
         let tag_name = node.tag_name().name();
 
-        if tag_name == "systeminfo" || tag_name == "machines" || tag_name == "machine"  {
+        if tag_name == "" || tag_name == "systeminfo" || tag_name == "machines" || tag_name == "machine" {
             continue;
-        } else if tag_name == "service" {
-            match node.attribute("status") {
-                None => eprintln!("Cannot get overall cluster status, attribute is missing"),
-                Some(status) =>
-                    println!("tableau_systeminfo status={} {}", status, get_epoch_ms())
-
-            }
         }
 
-        println!("{}", tag_name);
+        let status = node.attribute("status").unwrap_or("Unknown");
+
+        if tag_name == "service" {
+            println!("tableau_systeminfo,worker=all status=\"{}\" {}", status, get_epoch_ms())
+        } else {
+            let worker= node.attribute("worker").unwrap_or("Unknown");
+            println!("tableau_systeminfo,process=\"{}\",worker=\"{}\" status=\"{}\" {}"
+                     , tag_name
+                     , worker
+                     , status
+                     , get_epoch_ms());
+
+        }
     };
 
-   Ok(())
+    Ok(())
 }
 
 fn check_system_info(agent: &Agent, url: &String) {
-
     match get_system_info_xml(&agent, url) {
         Err(err) => eprintln!("Cannot fetch systeminfo.xml: {}", err.to_string()),
         Ok(xml) => {
